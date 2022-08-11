4a3ddb39ef7306a49d0ccc9ddb23c2183a1c8166
peterpeterparker <david.dalbusco@outlook.com>
2022-08-11 07:42:48 +0200

    feat: initial import

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tutorials 👋
+
+Sample repo for my articles about querying canister smart contracts with NodeJS scripts on the Internet Computer.
+
---
+++ src/manager_assets/src/index.js
@@ -0,0 +1,19 @@
+import { manager } from "../../declarations/manager";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await manager.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
